2669c5cfbf5ceb31d677e3bfcc60676ec4ea71a2
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2020-06-15 12:53:47 +0100

    serve static files and implement whitelist

--- mod.ts
+++ mod.ts
@@ -1,11 +1,39 @@
-import { Application } from "https://deno.land/x/oak@v4.0.0/mod.ts";
+import { Application, send } from "https://deno.land/x/oak@v4.0.0/mod.ts";
 
 const app = new Application();
 const port: number = 3333;
 
-app.use((ctx) => {
-  ctx.response.body = 'Hello From Oak';
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.response.headers.get('X-Response-Time');
+  console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`);
 });
 
-console.log('running on port ', port);
-await app.listen({ port });
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const delta = Date.now() - start;
+
+  ctx.response.headers.set('X-Response-Time', `${delta}ms`);
+});
+
+app.use(async (ctx) => {
+  const filePath = ctx.request.url.pathname;
+  const fileWhiteList = [
+    '/index.html',
+    '/javascripts/script.js',
+    '/images/favicon.png',
+    '/stylesheets/style.css'
+  ];
+
+  if(fileWhiteList.includes(filePath)){
+    await send(ctx, filePath, {
+      root: `${Deno.cwd()}/public`,
+    });
+  }
+});
+
+if (import.meta.main) {
+  console.log(`Server running on: http://localhost:${port}`);
+  await app.listen({ port });
+}
