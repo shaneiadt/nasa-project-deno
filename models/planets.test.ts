f449bbd8515fbc9e0eccf714cea63888aadbec0c
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2020-06-17 08:55:10 +0100

    unit test for filtering planets

---
+++ models/planets.test.ts
@@ -0,0 +1,49 @@
+import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
+import { filterHabitablePlanets } from './planets.ts';
+
+const HABITABLE_PLANET = {
+  koi_disposition: 'CONFIRMED',
+  koi_prad: '1',
+  koi_srad: '1',
+  koi_smass: '1'
+};
+
+const NOT_CONFIRMED = {
+  koi_disposition: 'FALSE POSITIVE'
+};
+
+const TO_LARGE_PLANETARY_RADIUS = {
+  koi_disposition: 'CONFIRMED',
+  koi_prad: '1.5',
+  koi_srad: '1',
+  koi_smass: '1'
+};
+
+const TO_LARGE_SOLAR_RADIUS = {
+  koi_disposition: 'CONFIRMED',
+  koi_prad: '1',
+  koi_srad: '1.01',
+  koi_smass: '1'
+};
+
+const TO_LARGE_SOLAR_MASS = {
+  koi_disposition: 'CONFIRMED',
+  koi_prad: '1',
+  koi_srad: '1',
+  koi_smass: '1.04'
+};
+
+Deno.test('filter only habitable planets', () => {
+
+  const result = filterHabitablePlanets([
+    HABITABLE_PLANET,
+    NOT_CONFIRMED,
+    TO_LARGE_PLANETARY_RADIUS,
+    TO_LARGE_SOLAR_MASS,
+    TO_LARGE_SOLAR_RADIUS
+  ]);
+
+  assertEquals(result, [
+    HABITABLE_PLANET
+  ]);
+});
--- models/planets.ts
+++ models/planets.ts
@@ -9,6 +9,17 @@ interface Planet {
 
 let planets: Planet[];
 
+export function filterHabitablePlanets(planets: Planet[]) {
+  return planets.filter(({ koi_disposition, koi_prad, koi_smass, koi_srad }) =>
+    koi_disposition === 'CONFIRMED'
+    && koi_prad > 0.5
+    && koi_prad < 1.5
+    && koi_smass > 0.78
+    && koi_smass < 1.04
+    && koi_srad > 0.99
+    && koi_srad < 1.01);
+}
+
 async function loadPlanetsData() {
   const path = join('data', 'kepler_exoplanets_nasa.csv');
   const file = await Deno.open(path);
@@ -20,14 +31,7 @@ async function loadPlanetsData() {
 
   Deno.close(file.rid);
 
-  const planets: Planet[] = result.filter(({ koi_disposition, koi_prad, koi_smass, koi_srad }) =>
-    koi_disposition === 'CONFIRMED'
-    && koi_prad > 0.5
-    && koi_prad < 1.5
-    && koi_smass > 0.78
-    && koi_smass < 1.04
-    && koi_srad > 0.99
-    && koi_srad < 1.01);
+  const planets: Planet[] = filterHabitablePlanets(result);
 
   return planets.map(planet => _.pick(planet, ['koi_prad', 'koi_smass', 'koi_srad', 'kepler_name', 'koi_count', 'koi_steff', 'koi_period']));
 }
