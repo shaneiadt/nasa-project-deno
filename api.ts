45e66892aab08ffe5fdafa9072062ea344212d75
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2020-06-18 09:49:06 +0100

    add launches endpoint

--- api.ts
+++ api.ts
@@ -1,5 +1,6 @@
 import { Router } from "https://deno.land/x/oak@v4.0.0/mod.ts";
 import * as planets from './models/planets.ts';
+import * as launches from './models/launches.ts';
 
 const router = new Router();
 
@@ -9,6 +10,18 @@ router.get("/", (ctx) => {
 
 router.get('/planets', (ctx) => {
   ctx.response.body = planets.getAllPlanets();
+});
+
+router.get('/launches', (ctx) => {
+  ctx.response.body = launches.getAll()
+});
+
+router.get('/launches/:id', (ctx) => {
+  if (ctx.params?.id) {
+    ctx.response.body = launches.getById(Number(ctx.params.id));
+  } else {
+    ctx.throw(404, "Launch Doe Not Exist");
+  }
 });
 
 export default router;
---
+++ models/launches.ts
@@ -0,0 +1,58 @@
+import * as log from "https://deno.land/std/log/mod.ts";
+import _ from "https://deno.land/x/deno_lodash/mod.ts";
+
+await log.setup({
+  handlers: {
+    console: new log.handlers.ConsoleHandler("DEBUG"),
+  },
+  loggers: {
+    default: {
+      level: "DEBUG",
+      handlers: ["console"],
+    },
+  }
+});
+
+interface Launch {
+  flightNumber: number;
+  mission: string;
+  rocket: string;
+  customers: string[];
+  launchDate: number;
+  upcoming: boolean;
+  success?: boolean;
+  target?: string;
+}
+
+const launches = new Map<number, Launch>();
+
+async function downloadLaunchData() {
+  log.info('Downloading Launch Data...');
+  const response = await fetch('https://api.spacexdata.com/v3/launches');
+  const data = await response.json();
+
+  for (const launch of data) {
+    const customers = _.flatMap(launch['rocket']['second_stage']['payloads'],
+      (payload: { customers: [] }) => payload.customers);
+
+    const flightData = {
+      flightNumber: launch['flight_number'],
+      mission: launch['mission_name'],
+      rocket: launch['rocket']['rocket_name'],
+      launchDate: launch['launch_date_unix'],
+      upcoming: launch['upcoming'],
+      success: launch['launch_success'],
+      customers,
+    };
+
+    launches.set(flightData.flightNumber, flightData);
+
+    if (import.meta.main) log.info(JSON.stringify(flightData));
+  }
+}
+
+await downloadLaunchData();
+
+export const getAll = () => Array.from(launches.values());
+
+export const getById = (id: number) => launches.has(id) ? launches.get(id) : null;
